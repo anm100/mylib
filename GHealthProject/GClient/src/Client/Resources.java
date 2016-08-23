498215c787634e92d0dc115b877e09e3cb07438a
Ahmad Mnasra <Ahmad Mnasra>
2016-08-23 13:34:00 +0300

    start

--- GHealthProject/GClient/src/Client/Resources.java
+++
@@ -1,9 +0,0 @@
-package Client;
-
-import javax.swing.ImageIcon;
-
-public class Resources {
-	public ImageIcon getIcon(String name){
-		return new ImageIcon(this.getClass().getResource("/img/" + name));
-		}
-}
